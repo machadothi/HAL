786ff676b23470dd5901cc37d1991cf54561a772
Thiago Machado Cunha <thiagomachado@Thiagos-MacBook-Air.local>
2022-11-05 22:19:08 +0100

    created gpio interface first draft

---
+++ Interfaces/IGpio.h
@@ -0,0 +1,44 @@
+#ifndef IGPIO_H_
+#define IGPIO_H_
+
+#include <cstdint>
+#include <string>
+
+namespace api{
+namespace hal{
+namespace gpio{
+
+enum class Direction
+{
+    UNKNOWN,
+    OUTPUT,
+    INPUT
+};
+
+enum class PinValue
+{
+    LOW,
+    HIGH
+};
+
+class IGPIO
+{
+public:
+    IGPIO();
+    ~IGPIO();
+    virtual void init(const std::string &name, 
+        const std::string &pin, 
+        Direction dir) = 0;
+
+    virtual PinValue read() = 0;
+
+    virtual void write(PinValue value) = 0;
+
+protected:
+};
+
+}; // namespace gpio
+}; // namespace hal
+}; // namespace api
+
+#endif /* IGPIO_H_ */

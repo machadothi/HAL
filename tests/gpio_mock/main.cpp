dce7520ccc72ddbe7882764893bc445691011665
Thiago Machado Cunha <thiagomachado@Thiagos-MacBook-Air.local>
2022-11-10 20:23:54 +0100

    priting out app arguments

--- tests/gpio_mock/main.cpp
+++ tests/gpio_mock/main.cpp
@@ -1,7 +1,14 @@
+#include <iostream>
+
 #include "gpio_mock.h"
 
 int main(int argc, char *argv[])
 {
+    for (int i = 0; i < argc; i++)
+    {
+        std::cout << "argv[" << i << "]: " << argv[i] << std::endl;
+    }
+
     api::hal::gpio::GpioMock gpio;
     gpio.init("gpio_mock", "pin_0", api::hal::gpio::Direction::OUTPUT);
     return 0;
